6d7125fca869a272f9d90f5141cc85949e9af4fd
Nicolas <nicolasrenovado12@gmail.com>
2025-09-23 02:04:50 -0300

    Adicionando normalização

---
+++ Normalização/1FN/teste_01/tabelaNormalizadaa.sql
@@ -0,0 +1,12 @@
+USE databasetest;
+
+CREATE TABLE usuario2 ( 
+	id_usuario INT PRIMARY KEY, 
+	nome varchar(30), 
+	senha varchar(30)
+)
+CREATE TABLE usuario_telefone ( 
+	id_usuario INT , 
+	telefone varchar(11),
+	FOREIGN KEY (id_usuario) REFERENCES usuario2(id_usuario)
+)
---
+++ Normalização/1FN/teste_01/tabelaNãoNormalizada.sql
@@ -0,0 +1,8 @@
+USE databasetest;
+
+CREATE TABLE usuario ( 
+	id_usuario INT, 
+	nome varchar(30), 
+	senha varchar(30), 
+	telefone varchar(11)
+)
