a0e1681389b563b0ad28ddb692a31fd6e79e64ef
Nicolas <nicolasrenovado12@gmail.com>
2025-11-03 00:01:55 -0300

    Adicionando funções agregadas, comecando com SUM

---
+++ FunçõesAgregadas/SUM.sql
@@ -0,0 +1,6 @@
+USE database1;
+INSERT INTO usuario2 VALUES ('nome_usuario', 'senha_usuario', 'G', 10, 13);
+INSERT INTO usuario2 VALUES ('nome_usuario', 'senha_usuario', 'G', 10, 13);
+INSERT INTO usuario2 VALUES ('nome_usuario', 'senha_usuario', 'G', 10, 13);
+INSERT INTO usuario2 VALUES ('nome_usuario', 'senha_usuario', 'G', 10, 13);
+INSERT INTO usuario2 VALUES ('nome_usuario', 'senha_usuario', 'G', 10, 13);
