52a6b1fd428b37bfc86fae5335fe8d6a6d70ddea
Nicolas <nicolasrenovado12@gmail.com>
2025-11-10 23:54:33 -0300

    Adicionando CONCAT e COUNT

---
+++ FunçõesAgregadas/COUNT.sql
@@ -0,0 +1,8 @@
+USE database1;
+SELECT COUNT(idade) FROM usuario2;
+
+/*
+COUNT conta quantos valores tem em todos
+os registros da coluna da tabela. Lembrando
+que ele não conta valores NULL
+*/
---
+++ FunçõesString/CONCAT.sql
@@ -0,0 +1,6 @@
+SELECT CONCAT(nome_usuario, nome2) FROM usuario2;
+
+/*
+O CONCAT junta dois valores das
+duas colunas.
+*/
